faefdd3c079ff3eadb41dadee0b34ab229bdc7e6
jannis <jannis@Odin-Project.myguest.virtualbox.org>
2023-05-02 13:57:05 +0200

    Improve validation + improve design of left sidebar

--- validation.js
+++ validation.js
@@ -1,12 +1,12 @@
-const submitButton = document.querySelector('button');
+// const submitButton = document.querySelector('button');
 
 console.log(firstPwField);
 console.log(secondPwField);
 
 
-submitButton.addEventListener('click', validateForm(firstPwField, secondPwField));
+// submitButton.addEventListener('click', validatePassword(firstPwField, secondPwField));
 
-function validateForm(firstPwField, secondPwField) {
+function validatePassword() {
         if (document.getElementById('password').value != document.getElementById('password2').value) {
             alert("The passwords don't match");
         } else {
